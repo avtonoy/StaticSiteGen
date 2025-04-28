8582d3220695e29420c673478548d8574519bebc
avton <avton@asdf.da>
2025-04-28 22:46:42 +0200

    block to block type

--- src/blocks.py
+++ src/blocks.py
@@ -3,7 +3,7 @@ from enum import Enum
 import re
 
 
-class BlockType(Enum): 
+class BlockType(Enum):
     PARAGRAPH = 'paragraph'
     HEADING = 'heading'
     CODE = 'code'
@@ -12,29 +12,65 @@ class BlockType(Enum):
     ORDERED_LIST = 'ordered_list'
 
 
+def markdown_to_block(markdown: str):
+    return list(map(lambda x: x.strip('\n').strip(' '), markdown.split('\n\n')))
 
 
-
-def markdown_to_block(markdown:str): 
-    return list(map(lambda x: x.strip('\n').strip(' '),markdown.split('\n\n')))
-
-
-def check_heading(block:str): 
-    if block[0] == '#': 
-        return None !=re.match(r'\#{1,6}\ {1}[a-zA-Z]',block)
+def check_heading(block: str):
+    if block[0] == '#':
+        return None != re.match(r'\#{1,6}\ {1}[a-zA-Z]', block)
     return False
 
-def check_code(block:str): 
-    return None !=re.match(r'^\`{3}.*?[\s\S]*\`{3}$',block)
 
-def block_to_block_type(block:str) -> BlockType: 
-    res = BlockType.PARAGRAPH
-    
-    if check_heading(block): 
+def check_code(block: str):
+    return None != re.match(r'^\`{3}.*?[\s\S]*\`{3}$', block)
+
+
+def check_quote(block: str):
+    lines = list(map(lambda x: x.strip(' '), block.split('\n')))
+    for line in lines:
+        if line[0] == '>':
+            pass
+        else:
+            return False
+    if len(lines) > 0:
+        return True
+
+
+def check_unordered_list(block: str):
+    lines = list(map(lambda x: x.strip(' '), block.split('\n')))
+    for line in lines:
+        if line[0:2] == '- ':
+            pass
+        else:
+            return False
+    if len(lines) > 0:
+        return True
+
+
+def check_ordered_list(block: str):
+    lines = list(map(lambda x: x.strip(' '), block.split('\n')))
+    for idx, line in enumerate(lines):
+        pattern = f'^{str(idx+1)}. '
+        if re.match(pattern, line) != None:
+            pass
+        else:
+            return False
+    if len(lines) > 0:
+        return True
+
+
+def block_to_block_type(block: str) -> BlockType:
+
+    if check_heading(block):
         return BlockType.HEADING
-    if check_code(block): 
+    if check_code(block):
         return BlockType.CODE
-    
-    
-    return res
-    
+    if check_quote(block):
+        return BlockType.QUOTE
+    if check_unordered_list(block):
+        return BlockType.UNORDERED_LIST
+    if check_ordered_list(block):
+        return BlockType.ORDERED_LIST
+
+    return BlockType.PARAGRAPH
--- src/test_blocks.py
+++ src/test_blocks.py
@@ -6,8 +6,8 @@ from blocks import *
 
 
 class TestInlineText(unittest.TestCase):
-    def test_markdown_to_block(self): 
-      md = """
+    def test_markdown_to_block(self):
+        md = """
  This is **bolded** paragraph
 
 This is another paragraph with _italic_ text and `code` here
@@ -16,23 +16,29 @@ This is the same paragraph on a new line
 - This is a list
 - with items
 """
-      blocks=markdown_to_block(md)
-      self.assertListEqual(
-        blocks,
-        [
-            "This is **bolded** paragraph",
-            "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
-            "- This is a list\n- with items",
-        ],
-    )
-              
-              
-    def test_block_to_block_type(self): 
-        print(block_to_block_type('### This is **bol\n ded** paragraph'))
-        print(block_to_block_type('```This is coded \n paragraph```'))
+        blocks = markdown_to_block(md)
+        self.assertListEqual(
+            blocks,
+            [
+                "This is **bolded** paragraph",
+                "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
+                "- This is a list\n- with items",
+            ],
+        )
 
-
-
+    def test_block_to_block_type(self):
+        self.assertEqual(block_to_block_type(
+            '### This is **bol\n ded** paragraph'), BlockType.HEADING)
+        self.assertEqual(block_to_block_type(
+            '```This is coded\n paragraph ```'), BlockType.CODE)
+        self.assertEqual(block_to_block_type(
+            '>asdfasdf  dasfa asdf\n >asdfasdf jkadsflask'), BlockType.QUOTE)
+        self.assertEqual(block_to_block_type(
+            '- first element \n - second element'), BlockType.UNORDERED_LIST)
+        self.assertEqual(block_to_block_type(
+            '1. first element \n2. Second Element'), BlockType.ORDERED_LIST)
+        self.assertEqual(block_to_block_type(
+            '1. first element \n 2.Second Element'), BlockType.PARAGRAPH)
 
 
 if __name__ == "__main__":
